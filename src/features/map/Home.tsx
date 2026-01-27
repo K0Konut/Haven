ccf7286c3b421ddbf57fc806dc0a1f9a9aaa43cf
sonaa212 <bhargaviramanadane1@gmail.com>
2026-01-27 22:15:33 +0100

    Home page update

--- src/features/map/Home.tsx
+++ src/features/map/Home.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <section className="space-y-4">
      <header>
         <h2 className="text-2xl font-bold">👋 Bonjour !</h2>
         <p className="text-sm text-zinc-400">Prêt pour une balade ?</p>
       </header>
+      
     </section>
   );
 }

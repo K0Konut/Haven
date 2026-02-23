00184186a0604906df049b9f7562289ce9a2fcad
ShifuRyan <edwinruban@et.esiea.fr>
2026-02-23 11:40:47 +0100

    test tokens et environnement pour build APK

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import.meta.env.VITE_MAPBOX_TOKEN;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
